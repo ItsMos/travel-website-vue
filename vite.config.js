941429c7f4155d2b9e5464084bad175290cc22d8
ItsMos <mospro@live.com>
2022-12-05 19:55:54 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    base: '/travel-website-vue/'
-  },
+  base: '/travel-website-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
