e2af76f682fb4511e09dcd34ffadcd878049b1f7
ItsMos <mospro@live.com>
2022-12-05 19:05:30 +0200

    + deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    base: '/travel-website-vue/'
+  },
   plugins: [vue()],
   resolve: {
     alias: {
